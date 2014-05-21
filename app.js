69ba2c7f0c797512c85cdaf3dec100329fb00617
Zhang Yanwu <xunmengsj@yeah.net>
2014-05-21 20:53:58 +0800

    [张砚武] 修改结构

--- app.js
+++ app.js
@@ -15,21 +15,12 @@ var app = express();
 // all environments
 app.set('port', process.env.PORT || 3000);
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
-app.use(express.favicon());
-app.use(express.logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded());
-app.use(express.methodOverride());
-app.use(app.router);
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'view')));
 
 // development only
 if ('development' == app.get('env')) {
-  app.use(express.errorHandler());
+//  app.use(express.errorHandler());
 }
-
-app.get('/', routes.index);
 app.get('/users', user.list);
 var safe = {safe: true};
 app.get('/add-city', function (req, res) {
@@ -81,6 +72,6 @@ app.get('/city', function (req, res) {
     })
 });
 
-app.listen(app.get('port'), function(){
+http.createServer(app).listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
 });
